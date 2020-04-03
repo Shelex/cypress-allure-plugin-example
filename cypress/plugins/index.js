2f9f9c4ea6e856b7484ee5deb744c7e230d98e00
Oleksandr Shevtsov <oleksandr.shevtsov@90poe.io>
2020-04-03 14:18:28 +0300

    [chore] prettier and eslint config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,15 +9,15 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-const AllureWriter = require('cypress-allure-plugin/writer')
+const AllureWriter = require('cypress-allure-plugin/writer');
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  AllureWriter(on)
-  return config
-}
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    AllureWriter(on);
+    return config;
+};
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,8 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import 'cypress-allure-plugin'
-import './commands'
+import 'cypress-allure-plugin';
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
